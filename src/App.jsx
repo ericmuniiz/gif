e9032f4962232e332e1f0c3844ae9ff6b25df86a
ericmuniiz <107154288+ericmuniiz@users.noreply.github.com>
2023-11-13 21:44:36 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Player } from '@lottiefiles/react-lottie-player'
+
+function App() {
+  return (
+    <>
+      <Player
+        src="https://lottie.host/3d5e86ec-5709-4eef-b633-01509342cf3b/AaRmJeuK36.json" 
+        loop autoplay
+        speed={1} 
+        style={{ height: "300px", width: "300px"}}
+      />
+    </>
+  )
+}
+
+export default App;
